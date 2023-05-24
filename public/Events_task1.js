75cc896feef5898a7b09e4f9a0b267cf624735ae
bifurcatum <130781409+bifurcatum@users.noreply.github.com>
2023-05-24 23:25:09 +0300

    Task DOM 1

--- public/Events_task1.js
+++ public/Events_task1.js
@@ -1,7 +1,6 @@
 function hideSelf() {
     let button= document.querySelector('[class="hide-self-button"]');
     button.addEventListener('click', () => button.hidden = true);
-    // button.addEventListener('click', () => button.hidden = false);
 }
 
 hideSelf();
---
+++ public/Events_task2.js
@@ -0,0 +1,7 @@
+function toggleText() {
+    let button= document.querySelector('[class="toggle-text-button"]');
+    let hideText= document.getElementById("text");
+    button.addEventListener('click', () => hideText.toggleAttribute('hidden'));
+}
+
+toggleText();
